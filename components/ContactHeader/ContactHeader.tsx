fea413132d2aa2a1336fe492fc39c987cb12d2e8
Jishaal Kalyan <jishaal@gmail.com>
2022-02-06 09:44:43 +1300

    Fix eslint

--- components/ContactHeader/ContactHeader.tsx
+++ components/ContactHeader/ContactHeader.tsx
@@ -1,6 +1,6 @@
 export const ContactHeader = () => {
   return (
-    <header className="flex items-center bg-white h-16 px-6 bg-opacity-[.97]">
+    <header className="flex h-16 items-center bg-white bg-opacity-[.97] px-6">
       <p className="">
         For bookings, contact us on 021 1066 077 or{" "}
         <a href="mailto:bookings@bigl.co.nz?Subject=Big%20L%20site%20booking%20enquiry&amp;Body=%5BPlease%20include%20a%20phone%20number%20with%20your%20email%20so%20we%20can%20contact%20you%5D">

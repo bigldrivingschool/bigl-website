a2e2c8cd2e7fd106a16b447c2901d7f738f5c171
Jishaal Kalyan <jishaal@gmail.com>
2022-09-10 15:15:32 +1200

    Integrate send grid

--- pages/api/feedback.ts
+++ pages/api/feedback.ts
@@ -1,30 +1,42 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import sendgrid from "@sendgrid/mail";
 import { FeedbackFormData } from "../../models";
+
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY || "");
 
 type Data = {
   data: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  // Get data submitted in request's body.
   const body: FeedbackFormData = req.body;
 
-  // Optional logging to see the responses
-  // in the command line where next.js app is running.
   console.log("body: ", body);
 
-  // Guard clause checks for first and last name,
-  // and returns early if they are not found
   if (!body.name || !body.email || !body.feedbackAreas.length) {
-    // Sends a HTTP bad request error code
     return res.status(400).json({ data: "Required fields are missing" });
   }
 
-  // Found the name.
-  // Sends a HTTP success code
+  const msg = {
+    to: "testemail@outlook.com", // Change to your recipient
+    from: "testemail@gmail.com", // Change to your verified sender
+    subject: "Sending with SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+    mail_settings: {
+      sandbox_mode: {
+        enable: true,
+      },
+    },
+  };
+
+  const sendGridResp = await sendgrid.send(msg);
+
+  console.log(sendGridResp);
+
   res.status(200).json({ data: `${body.name} ${body.email}` });
 }
